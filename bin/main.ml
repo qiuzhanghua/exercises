018c7929768df23a5e76977ecb062906ee43a1b9
qiuzhanghua <qiuzhanghua@icloud.com>
2023-12-18 09:26:27 +0800

    feat: encode

--- bin/main.ml
+++ bin/main.ml
@@ -95,3 +95,42 @@ let () =
         [ "d" ];
         [ "e"; "e"; "e" ];
       ])
+
+let encode lst =
+  let rec aux count acc = function
+    | [] -> []
+    | [ x ] -> (count + 1, x) :: acc
+    | a :: (b :: _ as t) ->
+        if a = b then aux (count + 1) acc t else aux 0 ((count + 1, a) :: acc) t
+  in
+  List.rev (aux 0 [] lst)
+
+let () =
+  assert (
+    encode [ "a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e" ]
+    = [ (4, "a"); (1, "b"); (2, "c"); (2, "a"); (1, "d"); (3, "e") ])
+
+type 'a rle = One of 'a | Many of int * 'a
+
+let encode2 lst =
+  let rle count x = if count = 1 then One x else Many (count, x) in
+  let rec aux count acc = function
+    | [] -> []
+    | [ x ] -> rle (count + 1) x :: acc
+    | a :: (b :: _ as t) ->
+        if a = b then aux (count + 1) acc t
+        else aux 0 (rle (count + 1) a :: acc) t
+  in
+  List.rev (aux 0 [] lst)
+
+let () =
+  assert (
+    encode2 [ "a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e" ]
+    = [
+        Many (4, "a");
+        One "b";
+        Many (2, "c");
+        Many (2, "a");
+        One "d";
+        Many (3, "e");
+      ])
