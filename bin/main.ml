7b818f92c28e7763ac77023dec989adb68e7bd5e
qiuzhanghua <qiuzhanghua@icloud.com>
2023-12-20 12:19:46 +0800

    feat: gray

--- bin/main.ml
+++ bin/main.ml
@@ -549,3 +549,15 @@ let () =
         (false, true, true);
         (false, false, true);
       ])
+
+let gray n =
+  let rec aux k l =
+    if k < n then
+      aux (k + 1)
+        (List.map (fun x -> "0" ^ x) l
+        @ List.map (fun x -> "1" ^ x) (List.rev l))
+    else l
+  in
+  aux 1 [ "0"; "1" ]
+
+let () = Printf.printf "%s\n" (String.concat ";" (gray 3))
